bfabcc6e8f2701a521df60aa4b147a0fc7b1c97d
Vladimir Kuznetsov <mistakster@gmail.com>
2015-05-16 20:56:43 +0500

    add plugin code

---
+++ index.js
@@ -0,0 +1,24 @@
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-unwrap-at-media"', function (opts) {
+	return function (css) {
+		css.eachAtRule(function (atRule) {
+			var i, len, node;
+			if (atRule.name != 'media') {
+				return;
+			}
+			if (atRule.nodes && atRule.nodes.length) {
+				len = atRule.nodes.length - 1;
+				for (i = len; i >=0; i--) {
+					node = atRule.nodes[i];
+					if (i === len)
+						node.after = (node.after || '') + (atRule.after || '');
+					if (i === 0)
+						node.before = (atRule.before || '') + (node.before || '');
+					atRule.parent.insertAfter(atRule, node);
+				}
+			}
+			atRule.removeSelf();
+		});
+	};
+});
