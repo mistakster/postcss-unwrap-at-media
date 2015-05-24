82ef56136d0f3b31826f759ac072638ce2f23ec4
Vladimir Kuznetsov <mistakster@gmail.com>
2015-05-24 18:26:37 +0500

    disallow some media rules

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var postcss = require('postcss');
+var _ = require('lodash');
+
+function normalizeArray(arr) {
+	if (!_.isArray(arr)) {
+		arr = [arr];
+	}
+	return arr.map(function (value) {
+		return '' + value;
+	});
+}
+
+function isAllowed(list, params) {
+	return !(list && list.some(function (value) {
+		return value == params;
+	}));
+}
 
 module.exports = postcss.plugin('postcss-unwrap-at-media"', function (opts) {
+	opts = opts || {};
+	if (opts.disallow) {
+		opts.disallow = normalizeArray(opts.disallow);
+	}
 	return function (css) {
 		css.eachAtRule('media', function (atRule) {
 			var i, len, node;
-			if (atRule.nodes && atRule.nodes.length) {
+			if (isAllowed(opts.disallow, atRule.params) && atRule.nodes && atRule.nodes.length) {
 				len = atRule.nodes.length - 1;
 				for (i = len; i >= 0; i--) {
 					node = atRule.nodes[i];
--- test/index.js
+++ test/index.js
@@ -20,4 +20,22 @@ describe('unwrap @media', function () {
 		css.should.be.equal('p {\n    margin: 1em 0;\n}\n\ndiv {\n    color: red;\n}');
 	});
 
+	it('should delete a block with particular @media rules', function () {
+		var css = postcss([plugin({disallow: 'print'})]).process(
+			'@media print {\n    div {\n        color: red;\n    }\n}\n\n' +
+			'@media screen {\n    div {\n        color: red;\n    }\n}\n\n' +
+			'p {\n    margin: 1em 0;\n}'
+		).css;
+		css.should.be.equal('div {\n    color: red;\n}\n\np {\n    margin: 1em 0;\n}');
+	});
+
+	it('should delete a block with any of specified @media rules', function () {
+		var css = postcss([plugin({disallow: ['print', 'screen']})]).process(
+			'@media print {\n    div {\n        color: red;\n    }\n}\n\n' +
+			'@media screen {\n    div {\n        color: red;\n    }\n}\n\n' +
+			'p {\n    margin: 1em 0;\n}'
+		).css;
+		css.should.be.equal('p {\n    margin: 1em 0;\n}');
+	});
+
 });
