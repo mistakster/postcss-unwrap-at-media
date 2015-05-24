2eb5c6608ab6cbae420d60f7446c79f2b95df31c
Vladimir Kuznetsov <mistakster@gmail.com>
2015-05-24 16:53:22 +0500

    format code

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ module.exports = postcss.plugin('postcss-unwrap-at-media"', function (opts) {
 			}
 			if (atRule.nodes && atRule.nodes.length) {
 				len = atRule.nodes.length - 1;
-				for (i = len; i >=0; i--) {
+				for (i = len; i >= 0; i--) {
 					node = atRule.nodes[i];
-					if (i === len)
+					if (i === len) {
 						node.after = (node.after || '') + (atRule.after || '');
-					if (i === 0)
+					}
+					if (i === 0) {
 						node.before = (atRule.before || '') + (node.before || '');
+					}
 					atRule.parent.insertAfter(atRule, node);
 				}
 			}
